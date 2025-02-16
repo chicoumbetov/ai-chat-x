512060f79f7907cb69d8039dbe13a68ba9c97ebd
chicoumbetov <shyngysmaaan@gmail.com>
2025-02-16 17:20:47 +0100

    :clown_face: mock data added

---
+++ src/constants/mock-data/category-mock-data.ts
@@ -0,0 +1,60 @@
+import { IMAGES } from '../IMAGES';
+
+export const categoryMockData = {
+    initial:[
+        {
+            name:'Create image',
+            icon:IMAGES.imageIcon,
+        },
+        {
+            name:'Make a Plan',
+            icon:IMAGES.bulbIcon,
+        },
+        {
+            name:'Brainstorm',
+            icon:IMAGES.bulbIcon,
+        },
+    ],
+    all:[
+        {
+            name:'Create image',
+            icon:IMAGES.imageIcon,
+        },
+        {
+            name:'Make a Plan',
+            icon:IMAGES.bulbIcon,
+        },
+        {
+            name:'Brainstorm',
+            icon:IMAGES.bulbIcon,
+        },
+        {
+            name:'Analyze images',
+            icon:IMAGES.analyzeIcon,
+        },
+        {
+            name:'Get advice',
+            icon:IMAGES.scholarIcon,
+        },
+        {
+            name:'code',
+            icon:IMAGES.codeIcon,
+        },
+        {
+            name:'Make a plan',
+            icon:IMAGES.bulbIcon,
+        },
+        {
+            name:'Suprise me',
+            icon:IMAGES.walletIcon,
+        },
+        {
+            name:'Summarize text',
+            icon:IMAGES.paperIcon,
+        },
+        {
+            name:'Help me write',
+            icon:IMAGES.pencil_2Icon,
+        },
+    ],
+};
---
+++ src/constants/mock-data/contextmenu1mockdata.ts
@@ -0,0 +1,16 @@
+import { IMAGES } from '../IMAGES';
+
+export const contextMenu1MockData = [
+   {
+    title:'Rename',
+    icon:IMAGES.pencil_3Icon,
+   },
+   {
+    title:'Archive',
+    icon:IMAGES.archive2Icon,
+   },
+   {
+    title:'Delete',
+    icon:IMAGES.deleteIcon,
+   },
+];
