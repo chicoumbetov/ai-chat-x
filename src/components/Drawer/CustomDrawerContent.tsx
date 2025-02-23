443e1d3f084bf4732b1417cb4cbac7ebab0e9812
chicoumbetov <shyngysmaaan@gmail.com>
2025-02-23 21:09:28 +0100

    :sparkles: search in drawer

--- src/components/Drawer/CustomDrawerContent.tsx
+++ src/components/Drawer/CustomDrawerContent.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { useRouter } from "expo-router";
 import { View } from "react-native";
 
-import { Drawer, DrawerItem } from "@ui-kitten/components";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { chatListMockData } from "@/src/constants/mock-data/chatlistmockdata";
+import { Drawer } from "@ui-kitten/components";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
+import Animated, { useSharedValue, withTiming } from "react-native-reanimated";
+import { styles } from "../main-structure/styles";
+import NavigationItem from "../navigation-item";
+import SearchComponent from "../search-component";
 
 function findIndexByRouteName(name?: string) {
   switch (name) {
@@ -19,10 +27,14 @@ function findIndexByRouteName(name?: string) {
 
 const CustomDrawerContent = (props: any) => {
   const { state } = props;
+  const gestureHandlerWidth = useSharedValue(0);
 
   const [selectedIndex, setSelectedIndex] = React.useState();
   const inset = useSafeAreaInsets();
   const linkTo = useRouter();
+
+  const [defaultCategory, setDefaultCategory] = useState(true);
+  const [categoryAnimationFlag, setCategoryAnimationFlag] = useState(true);
 
   const onSelectProps = (index: any) => {
     switch (index.row) {
@@ -33,6 +45,12 @@ const CustomDrawerContent = (props: any) => {
       default:
         break;
     }
+  };
+
+  const onNewChatPress = () => {
+    gestureHandlerWidth.value = withTiming(0);
+    setCategoryAnimationFlag(!categoryAnimationFlag);
+    setDefaultCategory(true);
   };
 
   return (
@@ -51,7 +69,24 @@ const CustomDrawerContent = (props: any) => {
           }}
           {...props}
         >
-          <DrawerItem title="Chat" />
+          <Animated.View style={styles.navigationWrapper}>
+            <View style={styles.navigationContentWrapper}>
+              <ScrollView showsVerticalScrollIndicator={false}>
+                <SearchComponent onNewChatPress={onNewChatPress} />
+                {/*
+                TODO
+                NavigationHeading
+                NavigationItem
+                NavigationHeading
+                */}
+                <View>
+                  {chatListMockData?.map((title, idx) => {
+                    return <NavigationItem key={idx} title={title} />;
+                  })}
+                </View>
+              </ScrollView>
+            </View>
+          </Animated.View>
         </Drawer>
       </GestureHandlerRootView>
     </View>
